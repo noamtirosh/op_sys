ed9237f33ecfb1832a7639fb32c4c622eeb65f83
noamt <noam6tirosh@gmail.com>
2021-11-07 17:01:05 +0200

    add comment and fix errors

--- Ex1/Ex1/Son/main.c
+++ Ex1/Ex1/Son/main.c
@@ -7,40 +7,68 @@
 //function declaration TODO add functions
 void encode_massage(char* massage, const char* key, int massge_len);
 HANDLE CreateFileSimple(LPCSTR file_name, char mode);
+long read_from_file(LPCSTR file_name, long offset, LPVOID p_buffer, const DWORD buffer_len);
+long write_to_file(LPCSTR file_name, LPVOID p_buffer, const DWORD buffer_len);
 
 int main(int argc, char* argv[])
 {
 	/// <summary>
-	/// TODO add function summary
+	/// open file with message read 16 bytes after given offset, open key from file,encrypt massge with key
+	///  and write to the end of diffulet file [Encrypte_message.txt]
 	/// </summary>
-	/// <param name="argc"></param>
-	/// <param name="argv"></param>
+	/// <param name="argc">num of arguments (should be 3 )</param>
+	/// <param name="argv">name of file contain message to encrypt ,offset in bytes,name file contain the key</param>
 	/// <returns></returns>
 	if (argc != NUM_OF_INPUTS + 1)
 	{
 		printf("ERROR: Not enough input arguments");
 		return ERR_CODE;
 	}
-	const char* massage_file_path = argv[MASSAGE_FILE_IND];
+	//get relevent params 
+	const char* message_file_path = argv[MASSAGE_FILE_IND];
 	const char* key_file_path = argv[KEY_FILE_IND];
-	char read_buffer[FILE_BUFFER+1] = { 0 };
+	DWORD offset = atoi(argv[OFFSET_IND]);
+	//init buffers 
+	char read_buffer[FILE_BUFFER+1] = { 0 };//TODO do we need etra 1 char for /0 ?
 	char key_buffer[FILE_BUFFER+1] = { 0 };
-	long offset = atoi(argv[OFFSET_IND]); //TODO check if it's ok to use long
-	read_from_file(massage_file_path, offset, read_buffer, FILE_BUFFER);
-	read_from_file(key_file_path,0, key_buffer, FILE_BUFFER);
+	
+	if (FILE_BUFFER != read_from_file(message_file_path, offset, read_buffer, FILE_BUFFER))
+	{
+		printf("was not able to read message from file: %s", message_file_path);
+		return ERR_CODE;
+	}
+	if (FILE_BUFFER != read_from_file(key_file_path,0, key_buffer, FILE_BUFFER))
+	{
+		printf("was not able to read key from file: %s", key_file_path);
+		return ERR_CODE;
+	}
 	encode_massage(read_buffer, key_buffer, FILE_BUFFER);
-	write_to_file(OUT_FILE_NAME, read_buffer, FILE_BUFFER);
+	if (FILE_BUFFER != write_to_file(OUT_FILE_NAME, read_buffer, FILE_BUFFER))
+	{
+		printf("was not able to write encrypte message from file: %s", OUT_FILE_NAME);
+	}
 	return(SUCCESS_CODE);
 
 }
 
-
 long read_from_file(LPCSTR file_name ,long offset, LPVOID p_buffer, const DWORD buffer_len)
 {
+	/// <summary>
+	///  read message in given length from givven offset in bytes from beginning
+	// from given file to buffer 
+	/// </summary>
+	/// <param name="file_name">path to file to read</param>
+	/// <param name="offset"> offset in bytes from beginning </param>
+	/// <param name="p_buffer"> pointer to buffer to put message</param>
+	/// <param name="buffer_len">length of message to read</param>
+	/// <returns>num of bytes readed from file</returns>
 	HANDLE h_file = CreateFileSimple(file_name, 'r');
 	DWORD n_written = 0;
 	DWORD last_error ;
-
+	if (INVALID_HANDLE_VALUE == h_file)
+	{
+		return n_written;
+	}
 	//move file pointer in ofset
 	SetFilePointer(
 		h_file,	//handle to file
@@ -65,9 +93,20 @@ long read_from_file(LPCSTR file_name ,long offset, LPVOID p_buffer, const DWORD 
 }
 long write_to_file(LPCSTR file_name, LPVOID p_buffer,const DWORD buffer_len)
 {
+	/// <summary>
+	///  write message from p_buffer in len buffer_len to file 
+	/// </summary>
+	/// <param name="file_name">path to file to write the buffer</param>
+	/// <param name="p_buffer">pointer to buffer</param>
+	/// <param name="buffer_len"> the length of the buffer</param>
+	/// <returns> num of bytes written to file</returns>
 	HANDLE h_file = CreateFileSimple(file_name, 'w');
 	DWORD last_error;
 	DWORD n_written = 0;
+	if (INVALID_HANDLE_VALUE == h_file)
+	{
+		return n_written;
+	}
 	//move file pointer in ofset
 	SetFilePointer(
 		h_file,	//handle to file
@@ -90,22 +129,14 @@ long write_to_file(LPCSTR file_name, LPVOID p_buffer,const DWORD buffer_len)
 	CloseHandle(h_file);
 	return n_written;
 }
-
-DWORD get_file_len(LPCSTR file_name)
-{
-	HANDLE h_file = CreateFileSimple(file_name, 'r');
-	DWORD current_file_position;
-	//move file pointer in ofset
-	current_file_position = SetFilePointer(
-		h_file,	//handle to file
-		0, // number of bytes to move the file pointer
-		NULL, // 
-		FILE_END);
-	return current_file_position;
-}
-
 HANDLE CreateFileSimple(LPCSTR file_name,char mode)
 {
+	/// <summary>
+	/// use CreateFile with default params for read or write and return the handle
+	/// </summary>
+	/// <param name="file_name">file path</param>
+	/// <param name="mode">mode : 'r'/'R' for read acsses 'w'/'W' for write </param>
+	/// <returns>handle to file</returns>
 	HANDLE h_file;
 	DWORD last_error;
 	DWORD acsees;
@@ -116,44 +147,37 @@ HANDLE CreateFileSimple(LPCSTR file_name,char mode)
 	case 'r': case 'R':
 		acsees = GENERIC_READ;  // open for reading
 		share = FILE_SHARE_READ; // share for reading
-		creation_disposition = OPEN_EXISTING;
+		creation_disposition = OPEN_EXISTING; // existing file only
 		break;
 	case 'w':case'W':
 		acsees = GENERIC_WRITE;
 		share = FILE_SHARE_WRITE;
 		creation_disposition = OPEN_ALWAYS; // open if existe or create new if not
 		break;
-	case 'a':
-		acsees = GENERIC_READ | GENERIC_WRITE;
-		share = FILE_SHARE_WRITE | FILE_SHARE_READ;
-		creation_disposition = OPEN_ALWAYS; // open if existe or create new if not
-		break;
 	default:
 		acsees = GENERIC_READ;  // open for reading
 		share = FILE_SHARE_READ; // share for reading
-		creation_disposition = OPEN_EXISTING;
+		creation_disposition = OPEN_EXISTING;// existing file only
 	};
 	h_file = CreateFile(file_name,  // file to open
 		acsees,
 		share,
 		NULL,                  // default security
-		creation_disposition,  // existing file only
+		creation_disposition,  
 		FILE_ATTRIBUTE_NORMAL, // normal file
 		NULL);
 	if (h_file == INVALID_HANDLE_VALUE)
 	{
-		printf("problem");
+		printf("Unable to open file %s",file_name);
 		//TODO hendle problems when open file
 	}
 	last_error = GetLastError();
 	if (last_error == ERROR_FILE_NOT_FOUND)
 	{
-		printf("problem");
-
+		printf("did not find file %s", file_name);
 	}
 	return h_file;
 }
-
 void encode_massage(char *massage, const char *key,int massge_len)
 {
 	for (int i = 0; i < massge_len; i++)
